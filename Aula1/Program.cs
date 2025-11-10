ec2f1debbfb75667a78d057d17f5f0bcf25de718
Renata Werneck <rwmacedo@gmail.com>
2025-11-10 20:04:49 -0300

    commit inicial

---
+++ Aula1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite seu nome:");
+var nome = Console.ReadLine();
+
+Console.WriteLine($"Olá, ");
+Console.WriteLine(nome);
+
+Console.WriteLine("Digite a primeira nota:");
+var nota1=Console.ReadLine();
+Console.WriteLine("Digite a segunda nota:");
+var nota2 = Console.ReadLine();
+Console.WriteLine("Digite a terceira nota:");
+var nota3 = Console.ReadLine();
+
+var media = (Convert.ToInt32(nota1) + Convert.ToInt32(nota2)+Convert.ToInt32(nota3)) / 3;
+
+Console.Write($"A média das notas é: ");
+Console.Write(media);
